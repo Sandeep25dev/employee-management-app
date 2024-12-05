afd971aafbbd9a1422f3477021f6d0251c47d553
Sandeep25dev <sandeepkumar250903@gmail.com>
2024-12-05 08:27:25 +0000

    structured the employee dashboard layout

--- src/components/Dashboard/EmployeeDashboard.tsx
+++ src/components/Dashboard/EmployeeDashboard.tsx
@@ -1,13 +1,15 @@
 import Header from "../Layout/Header";
 import TaskListNumbers from "../Layout/TaskListNumbers";
 import TaskList from "../TaskList/TaskList";
+import { LoggedUserDataTypes } from "../../types/userDataProps";
 
-const EmployeeDashboard = () => {
+const EmployeeDashboard = ({ data }: { data: LoggedUserDataTypes }) => {
+  console.log(data);
   return (
     <div className="p-10 bg-[#1C1C1C] h-screen">
-      <Header />
-      <TaskListNumbers />
-      <TaskList />
+      <Header data={data} />
+      <TaskListNumbers data={data} />
+      <TaskList data={data} />
     </div>
   );
 };
